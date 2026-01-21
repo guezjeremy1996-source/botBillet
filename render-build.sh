8b39cbdb8f1fcb3cda096c378e4423150310e824
guezjeremy1996-source <guezjeremy1996@gmail.com>
2026-01-21 17:12:41 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,14 +2,8 @@
 # Mettre à jour les paquets
 apt-get update
 
-# Installer Chromium et le driver
-apt-get install -y chromium chromium-driver
-
-# Définir la variable d'environnement pour Selenium
-export CHROME_BIN=/usr/bin/chromium
-
-# Installer les dépendances Python
-pip install -r requirements.txt
+# Installer Chromium et ChromeDriver
+apt-get install -y chromium-browser chromium-chromedriver
 
 # Lancer le bot
 python bot.py
